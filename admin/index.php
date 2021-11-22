606ebbda4745bc4f8e2189e8324df4462c19077f
“JosueBRM” <josuederker2022@gmail.com>
2021-11-21 21:56:52 -0600

    Creo que ya esta el CRUD :pray:

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,48 @@
 <?php
+
+    /*echo "<pre>";
+    var_dump($_POST);
+    echo "</pre>";*/
+
+    //Importar la conexion
+    require '../includes/config/database.php';
+    $db = conectarDB();
+
+    //Escribir el query
+    $query = "SELECT * FROM VIDEOJUEGO";
     
+    //Consultar la base de datos
+    $resultadoConsulta = mysqli_query($db, $query);
+
+    //Muestra mensaje condicional
     $resultado = $_GET['resultado'] ?? null;
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        $id = $_POST['id'];
+        $id = filter_var($id, FILTER_VALIDATE_INT);
+
+        if($id){
+            //Eliminar el archivo
+            $query = "SELECT IMAGEN FROM VIDEOJUEGO WHERE ID_VIDEOJUEGO = ${id}";
+            
+            $resultado = mysqli_query($db, $query);
+            $videojuego = mysqli_fetch_assoc($resultado);
+
+            
+            unlink('../imagenes/' . $videojuego['IMAGEN']);
+
+            //Eliminar el videojuego
+            $query = "DELETE FROM VIDEOJUEGO WHERE ID_VIDEOJUEGO = ${id}";
+            
+            $resultado = mysqli_query($db, $query);
+
+            if($resultado){
+                header('location: /admin?resultado=3');
+            }
+        }
+    }
+
+    //Incluye un template
     require '../includes/funciones.php';
     incluirTemplate('header');
 ?>
@@ -9,11 +51,53 @@
         <h1>Administrador de Tienda</h1>
         <?php if($resultado == 1): ?>
             <p class = "alerta exito">Videojuego añadido correctamente.</p>
+        <?php elseif($resultado == 2): ?>
+            <p class = "alerta exito">Videojuego actualizado correctamente.</p>
+        <?php elseif($resultado == 3): ?>
+            <p class = "alerta exito">Videojuego eliminado correctamente.</p>
         <?php endif; ?>
 
         <a class="botonNueva" href="/admin/propiedades/crear.php">Nuevo videojuego</a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody> <!--Mostrar los resultados-->
+                <?php while( $videojuego = mysqli_fetch_assoc($resultadoConsulta)): ?>
+
+                <tr>
+                    <td><?php echo $videojuego['ID_VIDEOJUEGO']; ?></td>
+                    <td><?php echo $videojuego['NOMBRE']; ?></td>
+                    <td> <img src="/imagenes/<?php echo $videojuego['IMAGEN'];?>" class="imagen-tabla"></td>
+                    <td> $ <?php echo $videojuego['PRECIO']; ?></td>
+                    <td>
+                        <form method="POST" class="w-100">
+                            <input type="hidden" name="id" value="<?php echo $videojuego['ID_VIDEOJUEGO']?>">
+
+                            <input type="submit" class="boton-rojo-block" value="Eliminar">
+                        </form>
+                        
+                        <a href="/admin/propiedades/actualizar.php?id=<?php echo $videojuego['ID_VIDEOJUEGO']; ?>" 
+                        class="boton-verde-block">Actualizar</a>
+                    </td>
+                </tr>
+                <?php endwhile ?>
+
+            </tbody>
+        </table>
     </main>
 
 <?php
+    //Cerrar la conexion
+    mysqli_close($db);
+
     incluirTemplate('footer');
 ?>
--- admin/propiedades/actualizar.php
+++ admin/propiedades/actualizar.php
@@ -1,10 +1,205 @@
 <?php
+
+    //Validar que sea un id valido
+    $id = $_GET['id'];
+    $id = filter_var($id, FILTER_VALIDATE_INT);
+
+    if(!$id){
+        header('Location: /admin');
+    }
+
+
+    //Base de datos
+    require '../../includes/config/database.php';
+    $db = conectarDB();
+
+    //Consulta para obtener los datos del videojuego
+    $consulta = "SELECT * FROM VIDEOJUEGO WHERE ID_VIDEOJUEGO = ${id}";
+    $resultado = mysqli_query($db, $consulta);
+    $videojuego = mysqli_fetch_assoc($resultado);
+
+    /*echo "<pre>";
+    var_dump($videojuego);
+    echo "</pre>";*/
+
+    //Consultar para obtener las clasificaciones
+    $consulta = "SELECT * FROM CLASIFICACION";
+    $resultado = mysqli_query($db, $consulta);
+    
+    //Arreglo con mensajes de errores 
+    $errores = [];
+
+    $titulo = $videojuego['NOMBRE'];
+    $precio = $videojuego['PRECIO'];
+    $descripcion = $videojuego['DESCRIPCION'];
+    $existencia = $videojuego['EXISTENCIA'];
+    $ID_clasificacion = $videojuego['ID_CLASIFICACION'];
+    $imagenVideojuego = $videojuego['IMAGEN'];
+
+    
+    //Ejecutar el codigo despues de que el usuario envie el formulario
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        /*echo "<pre>";
+        var_dump($_POST);
+        echo "</pre>";*/
+
+        /*echo "<pre>";
+        var_dump($_FILES);
+        echo "</pre>";*/
+
+        $titulo =  mysqli_real_escape_string( $db, $_POST['titulo'] );
+        $precio = (int)mysqli_real_escape_string( $db, $_POST['precio'] );
+        $descripcion = mysqli_real_escape_string( $db, $_POST['descripcion'] );
+        $existencia = (int)mysqli_real_escape_string( $db, $_POST['existencia'] );
+        $ID_clasificacion = (int)mysqli_real_escape_string( $db, $_POST['clasificacion'] );
+
+        //Asignar files hacia una imagen
+        $imagen = $_FILES['imagen'];
+
+        if(!$titulo){
+            $errores[] = "Debes añadir un titulo";
+        }
+
+        if(!$precio){
+            $errores[] = "El precio es obligatorio";
+        }
+
+        if( strlen( $descripcion ) < 50){
+            $errores[] = "La descripcion es obligatoria y debe tener al menos 50 caracteres";
+        }
+
+        if(!$existencia){
+            $errores[] = "Debe añadir obligatoriamente el numero de unidades en existencia";
+        }
+
+        if(!$ID_clasificacion){
+            $errores[] = "La clasificacion es obligatoria";
+        }
+
+        //Validar por tamaño(100KB maximo)
+        $medida = 1000 * 500;
+
+        if($imagen['size'] > $medida){
+            $errores[] = 'La imagen es muy pesada';
+        }
+        
+
+
+
+        /*echo "<pre>";
+        var_dump($errores);
+        echo "</pre>";*/
+
+        //Revisar que el arreglo de errores este vacio
+        if(empty($errores)){
+
+            //Crear carpeta 
+            $carpetaImagenes = '../../imagenes/';
+
+            if(!is_dir($carpetaImagenes)){
+                mkdir($carpetaImagenes);
+            }
+
+            $nombreImagen = '';
+            //Subida de archivos
+
+            if($imagen['name']){
+                //Eliminar la imagen previa
+                unlink($carpetaImagenes . $videojuego['IMAGEN']);
+
+            //Generar un nombre unico 
+            $nombreImagen = md5( uniqid( rand(), true ) ) . ".jpg";
+
+            //Subir la imagen
+            move_uploaded_file($imagen['tmp_name'], $carpetaImagenes . $nombreImagen );
+            }else{
+                $nombreImagen = $videojuego['IMAGEN'];
+            }
+
+
+
+
+
+
+            //Insertar en la base de datos
+            $query = " UPDATE VIDEOJUEGO SET NOMBRE = '${titulo}', PRECIO = ${precio}, IMAGEN = '${nombreImagen}', DESCRIPCION = '${descripcion}',
+             EXISTENCIA = ${existencia}, ID_CLASIFICACION = ${ID_clasificacion} WHERE ID_VIDEOJUEGO = ${id} ";
+
+            //echo $query;
+
+            
+
+            $resultado = mysqli_query($db, $query);
+            if($resultado){
+                //Redireccionar al usuario
+                header('Location: /admin?resultado=2');
+            }
+        }
+
+
+    }
+
     require '../../includes/funciones.php';
     incluirTemplate('header');
 ?>
 
     <main class = "contenedor">
-        <h1>Actualizar</h1>
+        <h1>Actualizar videojuego</h1>
+
+        <a class="botonNueva" href="/admin/index.php">Volver</a>
+
+        <?php foreach($errores as $error): ?>
+        <div class="alerta error">
+            <?php echo $error; ?>
+        </div>
+            
+        <?php endforeach; ?> 
+
+        <form class="formulario" method="POST" enctype="multipart/form-data">
+            <fieldset>
+                <legend>Informacion General</legend>
+
+                <label for="titulo">Titulo:</label>
+                <input type="text" id="titulo" name="titulo" placeholder="Titulo videojuego" value="<?php echo $titulo; ?>">
+
+                <label for="precio">Precio:</label>
+                <input type="number" id="precio" name="precio" placeholder="Precio videojuego" value="<?php echo $precio; ?>">
+
+                <label for="descripcion">Descripcion:</label>
+                <textarea id="descripcion" name="descripcion"><?php echo $descripcion; ?></textarea>
+
+                <label for="imagen">Imagen:</label>
+                <input type="file" id="imagen" accept="image/jpeg, image/png" name="imagen">
+
+                <img src="/imagenes/<?php echo $imagenVideojuego ?>" class = "imagen-small">
+
+            </fieldset>
+
+            <fieldset>
+                <legend>Stock Videojuego</legend>
+
+                <label for="existencia">Existencia:</label>
+                <input type="number" id="existencia" name="existencia" placeholder="Ej: 3" min="1" 
+                value="<?php echo $existencia; ?>">
+
+            </fieldset>
+
+            <fieldset>
+                <legend>Clasificacion</legend>
+                
+                <select name="clasificacion">
+                    <option value="">--Seleccione--</option>
+                    <?php while($row = mysqli_fetch_assoc($resultado) ) : ?>
+                        <option <?php echo $ID_clasificacion == $row['ID_CLASIFICACION'] ? 'selected' : ''; ?>  value="<?php echo $row['ID_CLASIFICACION']; ?>">
+                         <?php echo $row['CLASIFICACION']; ?> </option>
+
+
+                    <?php endwhile; ?>
+                </select>
+
+            </fieldset>
+            <input type="submit" value="Actualizar videojuego">
+        </form>
     </main>
 
 <?php
